00e4f0d5ce55af6ca21983a6fd4c07f52cc4b74a
yk <47151946@qq.com>
2018-03-31 14:59:44 +0800

    KNN算法识别手写数字

--- kNNTest.py
+++ kNNTest.py
@@ -1,11 +1,39 @@
 #!user/bin/env python
 # _*_ coding:utf-8 _*_
-
+from numpy import *
+import os
 import kNN
-group,labels = kNN.createDataSet()
-print(group)
-print(labels)
+def img2Vector(filename):
+    returnVect = zeros((1,1024))
+    fr = open(filename)
+    for i in range(32):
+        lineStr = fr.readline()
+        for j in range(32):
+            returnVect[0,32*i+j] = int(lineStr[j])
+    return returnVect
 
-print(kNN.classify0([0,0],group,labels,3))
-
-
+def handwritingClassTest():
+    hwLabels = []
+    trainingFileList = os.listdir('trainingDigits')
+    m = len(trainingFileList)
+    trainingMat = zeros((m,1024))
+    for i in range(m):
+        fileNameStr = trainingFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        hwLabels.append(classNumStr)
+        trainingMat[i,:] = img2Vector('trainingDigits/%s' % fileNameStr)
+    testFileList = os.listdir('testDigits')
+    errorCount = 0.0
+    mTest = len(testFileList)
+    for i in range(mTest):
+        fileNameStr = testFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        vectorUnderTest = img2Vector('testDigits/%s' % fileNameStr)
+        classifierResult = kNN.classify0(vectorUnderTest, trainingMat, hwLabels, 3)
+        print('the classifier came back with: %d, the real answer is: %d' % (classifierResult,classNumStr))
+        if(classifierResult != classNumStr):
+            errorCount += 1.0
+    print('\n the total number of errors is:%d' % errorCount)
+    print(']n the total error rate is: %f' % (errorCount/float(mTest)))
