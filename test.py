98f5e47b42fa0333a06fc62a8bc09ab25ef058f3
yk <47151946@qq.com>
2018-04-01 11:23:25 +0800

    修改文件目录结构

--- test.py
+++ test.py
@@ -1,12 +1,26 @@
 #!user/bin/env python
 # _*_ coding:utf-8 _*_
 
-def  add_END(L=[]):
-    L.append('END')
-    print(L)
-    return L
+from numpy import *
+import matplotlib.pyplot as plt
+import kNN
 
-# add_END([1,2,3])
+# group,labels = createDataSet()
+# print(group)
+# print(labels)
+# print(classify0([0,0],group,labels,3))
+# file2matrix('datingTestSet2.txt')
+datingDataMat,datingLabels = kNN.file2matrix('datingTestSet2.txt')
+# print(datingLabels)
 
-M = ['mich','tay','poi','bob','kok']
-M[:3]
+# Matplotlib对象简介
+# FigureCanvas  画布
+# Figure        图
+# Axes          坐标轴(实际画图的地方)
+fig = plt.figure()
+#add_subplot(xyz)，将画布分为x行y列，取第z块
+#add_subplot(x,y,z)，将画布分为x行y列，取第z块
+ax = fig.add_subplot(111)
+# 这里逗号分开两维，逗号前取行，逗号后取列
+ax.scatter(datingDataMat[:,0], datingDataMat[:,1], 15.0*array(datingLabels), 15.0*array(datingLabels))
+plt.show()
