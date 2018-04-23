dd6a75e2e9e35d206fa95e30fbd2f21f3e718289
MayYk <471519146@qq.com>
2018-04-23 21:19:48 +0800

    SMO算法辅助函数

--- svm/svmMLiA.py
+++ svm/svmMLiA.py
@@ -1,6 +1,38 @@
 #!user/bin/env python
 # _*_ coding:utf-8 _*_
+from numpy import *
 
+# 获取文件数据
+def loadDataSet(fileName):
+    dataMat = []
+    labelMat = []
+    fr = open(fileName)
+    for line in fr.readlines():
+        lineArr = line.strip().split('\t')
+        dataMat.append([float(lineArr[0]), float(lineArr[1])])
+        labelMat.append(float(lineArr[2]))
+    return dataMat,labelMat
+
+def selectJrand(i, m):
+    """
+
+    :param i:alpha的下标
+    :param m:alpha的数目
+    :return:
+    """
+    j = i
+    while(j == i):
+        j = int(random.uniform(0,m))
+    return j
+
+def clipAlpha(aj, H, L):
+    if aj > H:
+        aj = H
+    if L > aj:
+        aj = L
+    return aj
 
 if __name__ == '__main__':
+    dataArr, labelArr = loadDataSet('testSet.txt')
+    print(labelArr)
     pass
