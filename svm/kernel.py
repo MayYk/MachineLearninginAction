a9ad861d6e8ae66647f2498716cac2fe62b1c9a0
MayYk <471519146@qq.com>
2018-05-04 22:29:32 +0800

    手写识别

--- svm/kernel.py
+++ svm/kernel.py
@@ -109,6 +109,63 @@ def testRbf(k1 = 1.3):
         if sign(predict) != sign(labelArr[i]): errorCount += 1
     print('the test error rate is: %f' % (float(errorCount) / m))
 
+# 手写识别问题
+# 图像转向量
+def img2Vector(filename):
+    returnVect = zeros((1,1024))
+    fr = open(filename)
+    for i in range(32):
+        lineStr = fr.readline()
+        for j in range(32):
+            returnVect[0,32*i+j] = int(lineStr[j])
+    return returnVect
+
+def loadImage(dirName):
+    from os import listdir
+    hwLabels = []
+    trainingFileList = listdir(dirName)
+    m = len(trainingFileList)
+    trainingMat = zeros((m, 1024))
+    for i in range(m):
+        fileNameStr = trainingFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        if classNumStr == 9:
+            hwLabels.append(-1)
+        else:
+            hwLabels.append(1)
+        trainingMat[i,:] = img2Vector('%s/%s' % (dirName, fileNameStr))
+    return trainingMat, hwLabels
+
+def testDigits(kTup = ('rbf', 10)):
+    dataArr, labelArr = loadImage('trainingDigits')
+    b, alphas = svm.smoP(dataArr, labelArr, 200, 0.0001, 10000, kTup)
+    datMat = mat(dataArr)
+    labelMat = mat(labelArr).transpose()
+    svInd = nonzero(alphas.A > 0)[0]
+    sVs = datMat[svInd]
+    labelSV = labelMat[svInd]
+    print('there are %d Support Vectors' % shape(sVs)[0])
+    m,n = shape(datMat)
+    errCount = 0
+    for i in range(m):
+        kernelEval = kernelTrans(sVs, datMat[i,:], kTup)
+        predict = kernelEval.T * multiply(labelSV, alphas[svInd] + b)
+        if sign(predict) != sign(labelArr[i]):
+            errCount += 1
+    print('the training error rate is: %f' % (float(errCount)/m))
+    dataArr, labelArr = loadImage('testDigits')
+    errCount = 0
+    datMat = mat(dataArr)
+    labelMat = mat(labelArr).transpose()
+    m,n = shape(datMat)
+    for i in range(m):
+        kernelEval = kernelTrans(sVs, datMat[i,:], kTup)
+        predict = kernelEval.T * multiply(labelSV, alphas[svInd]) + b
+        if sign(predict) != sign(labelArr[i]):
+            errCount += 1
+    print('the test error rate is: %f' % (float(errCount)/m))
+
 if __name__ == '__main__':
-    testRbf()
-    pass
+    # testRbf()
+    testDigits(('rbf', 20))
