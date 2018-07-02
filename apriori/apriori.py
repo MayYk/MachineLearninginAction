e139ca4cdaf780aa2933c18459541c56e8acba62
MayYk <471519146@qq.com>
2018-07-02 16:45:05 +0800

    Apriori算法-毒蘑菇相似特征

--- apriori/apriori.py
+++ apriori/apriori.py
@@ -73,6 +73,60 @@ def apriori(dataSet, minSupport=0.5):
     return L, supportData
 
 
+def generateRules(L, supportData, minConf=0.7):
+    """
+
+    :param L:频繁项集列表
+    :param supportData:包含频繁项集列表支持数据的字典
+    :param minConf:最小可信度阈值
+    :return:
+    """
+    bigRuleList = []
+    for i in range(1, len(L)):
+        for freqSet in L[i]:
+            H1 = [frozenset([item]) for item in freqSet]
+            if i > 1:
+                rulesFromConSeq(freqSet, H1, supportData, bigRuleList, minConf)
+            else:
+                calcConf(freqSet, H1, supportData, bigRuleList, minConf)
+    return bigRuleList
+
+# 对规则进行评估
+def calcConf(freqSet, H, supportData, brl, minConf=0.7):
+    prunedH = []
+    for conseq in H:
+        conf = supportData[freqSet]/supportData[freqSet-conseq]
+        if conf >= minConf:
+            print(freqSet-conseq, '-->', conseq, 'conf:',conf)
+            brl.append((freqSet - conseq, conseq, conf))
+            prunedH.append(conseq)
+    return prunedH
+
+# 生成候选规则集合
+def rulesFromConSeq(freqSet, H, supportData, brl, minConf=0.7):
+    m = len(H[0])
+    if len(freqSet) > m+1:
+        Hmp1 = aprioriGen(H, m+1)
+        Hmp1 = calcConf(freqSet, Hmp1, supportData, brl, minConf)
+        if len(Hmp1) > 1:
+            rulesFromConSeq(freqSet, Hmp1, supportData, brl, minConf)
+
+# votesmart apikey正在等待审批
+# def votesmartData():
+#     votesmart.apikey = '123456789'
+
+def mushroomTest():
+    mushDataSet = [line.split() for line in open('mushroom.dat').readlines()]
+    L,supportData = apriori(mushDataSet, minSupport=0.3)
+
+    for item in L[1]:
+        if item.intersection('2'):
+            print(item)
+
+    for item in L[3]:
+        if item.intersection('2'):
+            print(item)
+
 
 def supTest():
     dataSet = loadDataSet()
@@ -85,13 +139,24 @@ def supTest():
 
 def aprioriTest():
     dataSet = loadDataSet()
-    L, suppData = apriori(dataSet)
+    L, supportData = apriori(dataSet)
     print(L[0])
     print(L[1])
     print(L[2])
     print(L[3])
 
+def rulesTest():
+    dataSet = loadDataSet()
+    L, supportData = apriori(dataSet, minSupport=0.5)
+
+    rules = generateRules(L, supportData, minConf=0.7)
+    print(rules)
+
+    rules = generateRules(L, supportData, minConf=0.5)
+    print(rules)
+
 if __name__ == '__main__':
     # supTest()
-    aprioriTest()
-
+    # aprioriTest()
+    # rulesTest()
+    mushroomTest()
